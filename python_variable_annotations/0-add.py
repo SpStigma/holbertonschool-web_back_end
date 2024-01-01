246013ca3b92c48dd2e98e653036aaa46f31335f
SpStigma <yoann.rivet0@gmail.com>
2024-01-01 01:43:19 +0100

    Fix: PEP8 validation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""This script defines a function 'add' that takes two floating-point numbers
+as arguments, adds them together, and returns the result."""
+
 
 def add(a: float, b: float) -> float:
     return a + b
