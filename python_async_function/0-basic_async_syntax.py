4d284a01b5fed2f07de4be3431c95491c943c0f1
SpStigma <yoann.rivet0@gmail.com>
2024-01-01 04:00:04 +0100

    First attempt of 0. The basics of async.

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""asynchronous coroutine """
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """waits for a random delay between 0 and max_delay and return it"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
