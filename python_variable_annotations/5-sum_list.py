7eca663f1e28ad03e10382266c0fea2c4aee7681
SpStigma <yoann.rivet0@gmail.com>
2024-01-01 02:40:42 +0100

    Fix: use of mudule typing instead of the standard nota.

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """script"""
 
+from typing import List
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """returns their sum as a float."""
     total = 0
     for number in input_list:
