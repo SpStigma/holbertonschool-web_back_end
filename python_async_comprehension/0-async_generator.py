a6b544fda3e0e75a1be3d2518384109f212fa5e4
SpStigma <yoann.rivet0@gmail.com>
2024-01-02 14:15:52 +0100

    Fix: PEP8 validator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-""""""
+"""Asynchronous generator that yields random numbers between 0 and 10
+with a 1-second interval."""
 import asyncio
 import random
 from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator:
+    """Yields random numbers asynchronously."""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
