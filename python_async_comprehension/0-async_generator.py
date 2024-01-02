7c92314e600dd4b7a5a73128ff77fe54755cfd59
SpStigma <yoann.rivet0@gmail.com>
2024-01-02 14:10:44 +0100

    First attempt of 0. Async Generator.

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""""""
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator:
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
