923da7ecf3e2d0f5f70744e55812e0bc742231bd
SpStigma <yoann.rivet0@gmail.com>
2024-01-04 09:10:28 +0100

    Fix: annotation.

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 with a 1-second interval."""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator:
+async def async_generator() -> Generator[float, None, None]:
     """Yields random numbers asynchronously."""
     for _ in range(10):
         await asyncio.sleep(1)
