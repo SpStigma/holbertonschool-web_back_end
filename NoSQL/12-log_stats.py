806898d27e60eb60a6437595e1ac538d0b82389c
SpStigma <yoann.rivet0@gmail.com>
2024-01-08 16:01:47 +0100

    First attempt of 12. Log stats.

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from pymongo import MongoClient
+
+def get_log_stats(nginx_collection):
+    """get logstat"""
+    total_logs = nginx_collection.count_documents({})
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_counts = {method: nginx_collection.count_documents({"method": method}) for method in methods}
+
+    # Status check count
+    status_check_count = nginx_collection.count_documents({"method": "GET", "path": "/status"})
+
+    return total_logs, method_counts, status_check_count
+
+def display_stats(total_logs, method_counts, status_check_count):
+    """display stat"""
+    print(f"{total_logs} logs")
+    print("Methods:")
+    for method, count in method_counts.items():
+        print(f"\tmethod {method}: {count}")
+    print(f"{status_check_count} status check")
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx = client.logs.nginx
+
+    total_logs, method_counts, status_check_count = get_log_stats(nginx)
+    display_stats(total_logs, method_counts, status_check_count)
