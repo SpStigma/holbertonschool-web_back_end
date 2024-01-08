09aefe055baff6eedc432c5fd7cdd538030d8323
SpStigma <yoann.rivet0@gmail.com>
2024-01-08 15:14:04 +0100

    Fix: documentation.

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -3,5 +3,5 @@
 
 
 def update_topics(mongo_collection, name, topics):
-    """"""
+    """function that changes all topics of a school document based on the name"""
     mongo_collection.update({"name": name}, {"$set": {"topics": topics}})
