55c08ab4a2805e9018ae54f2bb6827cc3af94041
SpStigma <yoann.rivet0@gmail.com>
2024-01-09 10:26:28 +0100

    Fix: PEP8 and documented.

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -28,6 +28,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Retrieve a specific page of data from the dataset."""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page > 0
 
@@ -36,7 +37,5 @@ class Server:
 
         if start is None or end is None:
             return []
-        
+  
         return dataset[start: end]
-
-
