70b0f1198a341ca818aca1422517501e1875df41
SpStigma <yoann.rivet0@gmail.com>
2024-01-24 11:47:31 +0100

    First attempt of 6. Handle multiple promises.

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+import signUpUser from './4-user-promise'
+import uploadPhoto from './5-photo-reject'
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      const promises = [];
+      results.forEach((promise) => {
+        promises.push({
+          status: promise.status,
+          value: promise.status === 'fulfilled' ? promise.value : `${promise.reason.name}: ${promise.reason.message}`,
+        });
+      });
+      return promises;
+    })
+}
