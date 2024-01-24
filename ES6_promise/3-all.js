b8495d5314cbac8d05e60ca7a83734dda76f7a17
SpStigma <yoann.rivet0@gmail.com>
2024-01-24 11:23:07 +0100

    Fix: import.

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,9 +1,7 @@
-/* eslint-disable */
-import uploadPhoto from './utils';
-import createUser from './utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
+  Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       const picture = values[0];
       const user = values[1];
