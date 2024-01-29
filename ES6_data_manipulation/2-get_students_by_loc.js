0a967c67a59e6171635536e6c859014a0ec7434a
SpStigma <yoann.rivet0@gmail.com>
2024-01-29 09:45:47 +0100

    Fix: eslint.

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
 export default function getStudentsByLocation(array, city) {
-    return array.filter(student => student.location === city);
+  return array.filter((student) => student.location === city);
 }
